780d63e3a823cf8c5698522534b53265a8871b59
Kirill Myagkov <kirillmyagkov17@gmail.com>
2020-05-21 23:19:57 +0300

    Prepare HTML templates

---
+++ src/assets/styles/index.js
@@ -0,0 +1,18 @@
+import './global.scss';
+
+import './components/ex-btn.scss';
+import './components/ex-input.scss';
+
+import './components/app.scss';
+import './components/app-header.scss';
+import './components/app-toolbar.scss';
+import './components/app-formula.scss';
+import './components/app-table.scss';
+
+import './components/dashboard.scss';
+import './components/dashboard-header.scss';
+import './components/dashboard-docs.scss';
+import './components/dashboard-chronology.scss';
+
+import './components/native-scroll.scss';
+import './components/container.scss';
